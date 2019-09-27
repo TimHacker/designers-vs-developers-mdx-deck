bb6c39d7502b16ec9b423c31cd3095671bf9411a
Tim Hacker <timothyhacker@gmail.com>
2019-09-27 06:42:25 +0100

    Added custom theme and reduced font size

---
+++ theme.js
@@ -0,0 +1,7 @@
+export default {
+  fonts: {
+    body: '"Helvetica Neue", Helvetica, Arial, Roboto, "Lucida Grande", sans-serif',
+    monospace: 'Menlo, Consolas, Courier, "Roboto Mono", monospace',
+  },
+  fontSizes: [8, 10, 12, 14, 20, 30, 40, 60, 80],
+}
